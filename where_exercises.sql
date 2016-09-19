969619c327e234786b08e5b9de2800763836362b
Ben Roberts <mikebenroberts@gmail.com>
2016-09-19 16:46:53 -0500

    MySQL exercises WHERE

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,4 +9,7 @@ SELECT * FROM employees
     
 SELECT * FROM employees	
     WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+    
+SELECT * FROM employees
+    WHERE birth_date LIKE '%-12-25';
 
