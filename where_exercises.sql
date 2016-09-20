a03ed60fc1ef9072b18229230a8f74c8eaddb79b
Ben Roberts <mikebenroberts@gmail.com>
2016-09-20 09:35:42 -0500

    MySQL exercises WHERE cont'd

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,8 +3,11 @@
 
 SELECT * FROM employees
     WHERE first_name = 'Irena'
+    AND gender = 'M'
     OR first_name =  'Vidya'
-    OR first_name =  'Maya';
+    AND gender = 'M'
+    OR first_name =  'Maya'
+    AND gender = 'M';
     
 SELECT * FROM employees
     WHERE last_name LIKE 'E%';
@@ -17,3 +20,19 @@ SELECT * FROM employees
 
 SELECT * FROM employees
     WHERE last_name LIKE '%q%';
+    
+SELECT * FROM employees
+    WHERE last_name LIKE 'e%'
+    OR last_name LIKE '%e';
+    
+SELECT * FROM employees
+    WHERE last_name LIKE 'e%'
+    AND last_name LIKE '%e';
+    
+SELECT * FROM employees
+    WHERE hire_date LIKE '199%'
+    AND birth_date LIKE '%-12-25';
+    
+SELECT * FROM employees
+    WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%';
