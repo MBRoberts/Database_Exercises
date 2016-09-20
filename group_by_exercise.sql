61217b68865a86ddb2c28eff43c97f45a421f59d
Ben Roberts <mikebenroberts@gmail.com>
2016-09-20 12:08:30 -0500

    MySQL exercises LIMIT OFFSET

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,6 +1,6 @@
 
-SELECT DISTINCT title FROM titles ORDER BY title;
+SELECT title FROM titles GROUP BY title;
 
-SELECT first_name, last_name FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%' GROUP BY first_name, last_name;
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'e%e' GROUP BY first_name, last_name;
 
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
