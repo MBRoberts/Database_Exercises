5d7fb98427fb07e809deec3560408f56839ef7d4
Ben Roberts <mikebenroberts@gmail.com>
2016-09-20 09:20:46 -0500

    MySQL exercises WHERE cont'd

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,9 @@
 
 
 SELECT * FROM employees
-    WHERE first_name IN('Irena', 'Vidya', 'Maya');
+    WHERE first_name = 'Irena'
+    OR first_name =  'Vidya'
+    OR first_name =  'Maya';
     
 SELECT * FROM employees
     WHERE last_name LIKE 'E%';
